ce1c857ee03681f2717bc5cec38bdc77dbfaca72
Samiul-7 <152017654+Samiul-7@users.noreply.github.com>
2024-09-11 19:49:39 +0600

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://grocer-ez-nine.vercel.app"
 
   return (
     <div>
